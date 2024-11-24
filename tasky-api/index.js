663c09106863a7832c35250204bda6b6ecd500c7
Elaine Coughlan <20067964@mail.wit.ie>
2024-11-24 14:29:56 +0000

    Mongo lab included Mongoose

---
+++ tasky-api/index.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import tasksRouter from './api/tasks';
+import './db';
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.use('/api/tasks', tasksRouter);
+
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+});
